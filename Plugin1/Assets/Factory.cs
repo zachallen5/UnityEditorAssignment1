55f19f391d0d9fbb3d97a692374b466ab87fb10e
Zach <zachary_allen@hotmail.com>
2019-10-02 21:50:57 -0400

    Item Spawner + 2d Overlay
    Items can now be spawned it by clicking the corresponding button, and then clicking on the map

--- Plugin1/Assets/Factory.cs
+++ Plugin1/Assets/Factory.cs
@@ -1,9 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+    
+
 
 public class Factory : MonoBehaviour
 {
+    public GameObject tree;
+    public GameObject flower;
+    public GameObject rock;
+    public Transform newPosition;
+    public Camera camera;
+    private bool isCarrying = false;
+    private GameObject objecttobeSpawned;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +26,64 @@ public class Factory : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isCarrying == true)
+        {
+
+   
+            objectSpawner(objecttobeSpawned);
+            Debug.Log(objecttobeSpawned);
+            
+        }
+
         
+    }
+
+    public void flowerSpawn()
+    {
+
+
+        objecttobeSpawned = flower;
+        
+        
+        isCarrying = true;
+        
+
+    }
+
+    public void rockspawn()
+    {
+        objecttobeSpawned = rock;
+        isCarrying = true;
+    }
+    
+    public void treeSpawn()
+    {
+        objecttobeSpawned = tree;
+        isCarrying = true;
+    }
+
+
+
+    public void objectSpawner(GameObject gameobject)
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            Ray raycast;
+            raycast = camera.ScreenPointToRay(mousePos);
+            RaycastHit hit;
+
+
+
+
+            if (Physics.Raycast(raycast, out hit, Mathf.Infinity))
+            {
+
+                Instantiate(gameobject, hit.point, Quaternion.identity);
+                isCarrying = false;
+            }
+        }
+
     }
 }
